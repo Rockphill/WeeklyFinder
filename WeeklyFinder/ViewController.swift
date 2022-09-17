266ff64fe80795a37e3c8a8993a54a018778d9ee
Andrei <macwin33@gmail.com>
2022-09-17 17:35:11 +0300

    add locale and checking input

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -16,18 +16,22 @@ class ViewController: UIViewController {
     
     @IBAction func findDay(_ sender: UIButton) {
         
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else { return}
+        
         let calendar = Calendar.current
         var dateComponents = DateComponents()
-        dateComponents.day = Int(dateTF.text!)
-        dateComponents.month = Int(monthTF.text!)
-        dateComponents.year = Int(yearTF.text!)
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
         
         let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
         dateFormatter.dateFormat = "EEEE"
         
-        let date = calendar.date(from: dateComponents)
+        guard let date = calendar.date(from: dateComponents) else { return}
         
-        let weekday = dateFormatter.string(from: date!)
+        let weekday = dateFormatter.string(from: date)
+        let capitalizedWeekday = weekday.capitalized
         
         resultLabel.text = weekday
     }
